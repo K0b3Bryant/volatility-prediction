6ea4f34e31b8e450da503ad977407a9fe1f8cc21
Marco <sejereje1011@gmail.com>
2025-01-07 09:39:47 +0100

    Update execution.py

--- execution.py
+++ execution.py
@@ -1,4 +1,6 @@
-### Parameters
+import optuna
+
+# Parameters
 split_years = [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022] # years to include
 train_years_num = 3                                                                   # size of train window
 test_years_num = 3                                                                    # size of test window
@@ -27,7 +29,7 @@ params = {'transaction_costs': 0.001, 'lag': 1, 'max_position': 1}              
 factors_list = ['Mkt-RF', 'SMB', 'HML', 'RMW', 'CMA', 'UMD', 'VIX', 'BAB']             # Factors for return analysis
 
 
-### Executetion
+# Executetion - Loop over defined years for average results
 main_msr_probs = []
 main_msr_metric = []
 main_feature_importance = []
@@ -35,7 +37,6 @@ main_prediction_metrics = []
 main_returns = []
 main_performance = []
 
-# Loop over defined years for average results
 for split_year in split_years:
     from datetime import datetime
     split_date = datetime(split_year, 1, 1)
@@ -96,3 +97,25 @@ print(buy_and_hold_df)
 # add sort
 #sorted_df = filtered_df.sort_values(by='Alpha', ascending=False)
 #print(sorted_df)
+
+# Find optimum SR
+def objective(trial):
+    # params
+    signal_name = trial.suggest_categorical('signal_name', list(signals.keys()))
+    signal_rolling_window = trial.suggest_int('signal_rolling_window', 5, 21)
+    strategy_func = trial.suggest_categorical('strategy_func', [mean_reversion, mean_momentum])
+
+    strategy = Strategy('Strategy', signals[signal_name], risky_returns, risk_free_returns, transaction_costs, max_position)
+    manager = StrategyManager(index=index, risk_free_returns=risk_free_returns)
+    manager.add_strategy(strategy, strategy_func, lag=lag, window=signal_rolling_window)
+    strategy_returns = manager.strategies['Strategy']['simple_returns']
+
+    excess_returns = strategy_returns - risk_free_returns
+    sharpe_ratio = excess_returns.mean() / excess_returns.std() * np.sqrt(250)
+
+    return sharpe_ratio
+
+study = optuna.create_study(direction='maximize')
+study.optimize(objective, n_trials=100)
+print(f"Best Sharpe Ratio: {study.best_value}")
+print(f"Best Parameters: {study.best_params}")
