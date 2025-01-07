3c21c84ce08b88bb058a56d8cc30d9eef97c7bc2
Marcoco <sejereje1011@gmail.com>
2025-01-07 08:40:41 +0100

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,19 @@
+def csv_reader(file_path: str):
+  """ Cleans CSV files """
+    if os.path.exists(file_path):
+        temp_df = pd.read_csv(file_path, nrows=0)
+        date_column = None
 
+        for col in temp_df.columns:
+            if col.lower() == 'date':
+                date_column = col
+                break
+
+        if date_column:
+            return pd.read_csv(file_path, parse_dates=[date_column], index_col=date_column)
+            print('file read with date column as index')
+        else:
+            print('file read raw')
+            return pd.read_csv(file_path)
+    else:
+        raise FileNotFoundError(f"File not found: {file_path}")
