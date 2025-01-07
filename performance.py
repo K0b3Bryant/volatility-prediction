ba8de67c60fd3664b601225197eff23f906e07c3
Marcoco <sejereje1011@gmail.com>
2025-01-07 08:54:34 +0100

    Update performance.py

--- performance.py
+++ performance.py
@@ -1,3 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
 def generate_performance_report(strategy_manager):
     # Creates performance metrics and visualizations
     pass
