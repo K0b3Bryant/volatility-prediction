32e56901d38d10887008d6bbff0d417ddd0c7281
Marcoco <sejereje1011@gmail.com>
2025-01-07 08:54:04 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,11 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from skorch import NeuralNetRegressor
+import torch
+import torch.nn as nn
+
 class PredictionManager:
     def __init__(self):
         self.models = {}
