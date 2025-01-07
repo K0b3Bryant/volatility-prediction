e5c01826d9b6ba30cc8638611dd8d3d14caa7b48
Marco <sejereje1011@gmail.com>
2025-01-07 09:35:48 +0100

    Update execution_loop.py

--- execution_loop.py
+++ execution_loop.py
@@ -1,55 +1,41 @@
-
-# params
-split_years = [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022] # 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022
-train_years_num = 3
-test_years_num = 3
-robustness_test = 'sliding window' # 'expanding window', 'sliding window'
-
-# Measures params
-window_size = 3
-high, low, close, open, = 'high_SPX', 'low_SPX', 'close_SPX', 'open_SPX'
-
-# MSR params
-probability = 'filtered_1'  # pred_smooth, filtered_1, ...  - try with filtered
-labels = ['p_3d'] # 'vol_3d', 'gk_3d', 'p_3d', 'gkyz_3d', 'diff_vol_3d', 'vov_3d'
-trends = ['c'] # 'ct', 'n',
-exogs = [['vix_SPX', 'iv_call_SPX']] # None, ['vix_SPX', 'iv_call_SPX']
-switching_variance = True
+### Parameters
+split_years = [2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022] # years to include
+train_years_num = 3                                                                   # size of train window
+test_years_num = 3                                                                    # size of test window
+robustness_test = 'sliding window'                                                    # type of window either 'expanding window' or 'sliding window'
+window_size = 3                                                                       # size of measures generation window
+high, low, close, open, = 'high_SPX', 'low_SPX', 'close_SPX', 'open_SPX'              # Define high, low, open, and close prices
+probability = 'filtered_1'                                                             # Type of probabilitites in a Hidden Markov Model or Markow Switching Model
+labels = ['p_3d']                                                                      # Which probabiltites to include
+trends = ['c']                                                                         # Types of trends
+exogs = [['vix_SPX', 'iv_call_SPX']]                                                   # Exogenous variables
+switching_variance = True                                                              # ...
 scale_endog = False
 rescale_endog= True # multiply by 100
 scale_exog = True
 em_iter = 1000
 search_reps = 100
-regimes = 2
+regimes = 2                                                                            # Number of underlying regimes
 show_model = True
-
-# prediciton
-lags = [1, 2, 3, 4, 5, 10, 21, 63]
-
-# Strategies params
-all_position_funcs = [Threshold] #MeanMomentum, MeanReversion, Threshold
+lags = [1, 2, 3, 4, 5, 10, 21, 63]                                                     # Number of parameter lags
+all_position_funcs = [Threshold]                                                       # Type of strategy either MeanMomentum, MeanReversion, or Threshold
 bound_sets = [
     {'upper': 0.99, 'lower': 0.99},
-    ]
-rolling_windows = [15]
-params = {'transaction_costs': 0.001, 'lag': 1, 'max_position': 1} # neutralized trading costs until finding alpha
-
-# Performance params
-factors_list = ['Mkt-RF', 'SMB', 'HML', 'RMW', 'CMA', 'UMD', 'VIX', 'BAB'] # cinsider cutting until finding alpha
+    ]                                                                                  # Boundries of strategy switches
+rolling_windows = [15]                                                                 # Size of window for stratgy switches
+params = {'transaction_costs': 0.001, 'lag': 1, 'max_position': 1}                     # Trading parameters including trading costs
+factors_list = ['Mkt-RF', 'SMB', 'HML', 'RMW', 'CMA', 'UMD', 'VIX', 'BAB']             # Factors for return analysis
 
 
-############################### Execution ######################################
-# store data
+### Executetion
 main_msr_probs = []
 main_msr_metric = []
-
 main_feature_importance = []
 main_prediction_metrics = []
-
 main_returns = []
 main_performance = []
 
-# date loop
+# Loop over defined years for average results
 for split_year in split_years:
     from datetime import datetime
     split_date = datetime(split_year, 1, 1)
@@ -58,30 +44,26 @@ for split_year in split_years:
         start_date = '2000-01-01'
     if robustness_test == 'sliding window':
         start_date = datetime(split_year-train_years_num, 1, 1)
-
-    # extract years
     if isinstance(start_date, str):
         start_year = datetime.strptime(start_date, '%Y-%m-%d').year
     else:
         start_year = start_date.year
-
     end_year = end_date.year+1
 
     print('Testing on:')
     print(start_date, start_year, split_date, split_year, end_date, end_year)
 
     # run main code
+    def main():
+        pass
 
-
-    # concat
+    # concat and export results
     main_msr_probs_merged = pd.concat(main_msr_probs, ignore_index=True)
     main_msr_metric_merged = pd.concat(main_msr_metric, ignore_index=True)
     main_feature_importance_merged = pd.concat(main_feature_importance, ignore_index=True)
     main_prediction_metrics_merged = pd.concat(main_prediction_metrics, ignore_index=True)
     main_returns_merged = pd.concat(main_returns, ignore_index=True)
     main_performance_merged = pd.concat(main_performance, ignore_index=True)
-
-    # export
     main_msr_probs_merged.to_csv('main_msr_probs_merged.csv', index=False)
     main_msr_metric_merged.to_csv('main_msr_metric_merged.csv', index=False)
     main_feature_importance_merged.to_csv('main_feature_importance_merged.csv', index=False)
